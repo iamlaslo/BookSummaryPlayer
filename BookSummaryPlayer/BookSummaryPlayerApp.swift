5e421f086043f9d2084a7b803683d680c43a2a99
Vlad Kozlov <v.kozlov@finik.pro>
2024-02-07 08:49:57 +0200

    Initial Commit

---
+++ BookSummaryPlayer/BookSummaryPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSummaryPlayerApp.swift
+//  BookSummaryPlayer
+//
+//  Created by Laslo Kozlov on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookSummaryPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
